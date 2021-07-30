37de4cfd200ba5b7bbe245898c302e6585ecfb45
vishal kumar <vkumar61@slb.com>
2021-07-30 16:25:05 +0530

    Working with 'ref's

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -28,17 +28,9 @@ const ErrorModal = (props) => {
   return (
     <React.Fragment>
       {ReactDOM.createPortal(
-        <Backdrop onConfirm={props.onConfirm} />,
-        document.getElementById("backdrop-root")
+        <Backdrop onConfirm={props.onConfirm} />, document.getElementById("backdrop-root")
       )}
-      {ReactDOM.createPortal(
-        <ModalOverlay
-          title={props.title}
-          message={props.message}
-          onConfirm={props.onConfirm}
-        />,
-        document.getElementById("modal-root")
-      )}
+      {ReactDOM.createPortal(<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm} />, document.getElementById('modal-root') )}
     </React.Fragment>
   );
 };
